33507a76780e82cc34c53fca3ee714630626cf02
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-03-20 16:09:04 -0400

    week1

--- live_review_sections/week1/CarInfo.java
+++ live_review_sections/week1/CarInfo.java
@@ -1,7 +1,25 @@
 package week1;
 
 public class CarInfo {
+	
+	public static void main(String[] args) {
+		int year = 2018;
+		String make = "BMW";
+		String model = "m3";
+		String transmission ="Automatic";
+		String color = "Red";
+		int miles = 2000;
+		double price = 50000.25;
+		
+		System.out.println(year+" "+make+" "+model+" "+transmission
+				+", "+color+" "+miles+" miles $"+price);
+		
+		
+	}
 
 }
+
+
+// 2010 Toyota Colrolla Automatic, White 25000 miles $20000
 
 //year make model transmission, color miles price
---
+++ live_review_sections/week1/Notes.java
@@ -0,0 +1,115 @@
+package week1;
+
+public class Notes {
+	
+	/*
+	 03/20/2020
+		Java Review:
+				Java Intro
+				IDE Tools
+				print statements
+				escape sequences 
+				variables
+				Arithmetic Operators: -, +, *, /, %
+		
+		
+		Jave: programming langauge
+		
+		IDE: Integrated Development Environment
+							Eclipse
+							IntelliJ
+							Visual Studio 
+							Visual Studio Code
+							NetBeans
+		
+		How to find package explorer in eclipse:
+				windown -> show view
+		
+		
+		How to find console in eclipse:
+				windown -> show view	
+		
+		
+		Font
+			Command +
+			Command -
+		
+			Ctrl +  or Ctrl Shift +
+			Ctrl -  or Ctrl Shift -
+		
+		
+		How to create project:
+				1. file -> new -> Java Project
+				2. Type "ProjectName"
+				3. Click Finish Button
+				4. Click "DO Not create module"
+		
+		
+		src: source folder, used for storing packages
+				
+				right click PROJECT -> new  -> source folder
+		
+		package: used for storing java files (class etc) in organized manner
+		
+				right click SOURCE FOLDER -> new -> package
+		
+		class: where we will be writing our java codes
+				
+				right click PACKAGE -> new -> class
+		
+		main: makes the class runnable
+		
+				java executez the codes in main method from top to buttom
+				
+		
+		print statements:
+				System.out.println(); appends new line at the end
+				System.out.print(); does not append new line at the end
+		
+				shortcut: 1. type "syso"
+						  2. Ctrl + Space
+		
+		escape sequences:
+			\t: paragraph space
+			\": prints "
+			\n: appends new line
+			\\: prints \
+		
+		
+		 create a class called TellMeAboutYourSelf
+		        print:
+		            your name
+		            where do you live
+		            what do you do
+		            what's your dream job
+		
+		
+		
+		4 importat primitives:
+				int: whole numbers
+				double: decimal number
+				char: '', for single characters
+				boolean: true & false expressions
+		
+		
+		String: text, for sequences of characters, ""
+		
+		
+		
+		concatenation VS Addition
+				
+				addition:
+						number + number
+		
+				concatenation:
+						text + number
+						number + text
+						text + text
+		
+		
+		
+
+	 */
+	
+
+}
