17148fd412b7f6642021fe70f2dd43acdca86425
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-03-20 13:07:15 -0400

    Week1

--- live_review_sections/week1/Shapes.java
+++ live_review_sections/week1/Shapes.java
@@ -2,6 +2,34 @@ package week1;
 
 public class Shapes {
 
+	public static void main(String[] args) {
+		
+		System.out.println("    ^");
+		System.out.println("   / \\");
+		System.out.println("  /   \\");
+		System.out.println(" /     \\");
+		System.out.println("/       \\");
+		System.out.println("---------");
+		
+		System.out.println("\n---------------------------------");
+		
+		System.out.println("\t    ^");
+		System.out.println("\t   / \\");
+		System.out.println("\t  /   \\");
+		System.out.println("\t /     \\");
+		System.out.println("\t/       \\");
+		System.out.println("\t---------");
+		System.out.println("\t\\       /");
+		System.out.println("\t \\     /");
+		System.out.println("\t  \\   /");
+		System.out.println("\t   \\ /");
+		System.out.println("\t    v");
+
+		
+		
+	}
+	
+	
 }
 
 /*
