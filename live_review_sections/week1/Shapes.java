2fbccee81f54f0a895a4973b4dd7c123d791877b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-03-20 12:53:54 -0400

    Week1

---
+++ live_review_sections/week1/Shapes.java
@@ -0,0 +1,43 @@
+package week1;
+
+public class Shapes {
+
+}
+
+/*
+ task: print the following shape on your console
+
+        ^
+       / \
+      /   \
+     /     \
+    /       \
+    ---------
+    Triangle
+
+task2: print the following shape on your console
+        ^
+       / \
+      /   \
+     /     \
+    /       \
+    ---------
+    \       /
+     \     / 
+      \   /
+       \ /
+        v  
+Diamond
+task3: print the following shape on your console
+        ^
+       / \
+      / * \
+     / * * \
+    / * * * \
+    ---------
+    \ * * * /
+     \ * * / 
+      \ * /
+       \ /
+        v  
+ */
