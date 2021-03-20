065644bce856990266b9c6bd3adf5417caf3d408
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-03-20 15:14:10 -0400

    week1

--- live_review_sections/week1/VariablesPractice.java
+++ live_review_sections/week1/VariablesPractice.java
@@ -3,3 +3,14 @@ package week1;
 public class VariablesPractice {
 
 }
+
+/*
+ Task01. create a class called EmployeeInfo:
+  1. declare the following variables: name, companyName, employeeId, jobTitle, salary 
+  2. print:
+        your company's name
+        your Name
+        your employee Id
+        your job title
+        your salary
+ */
