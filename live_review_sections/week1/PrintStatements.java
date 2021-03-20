b9ddcc79d105495624c64e4c3b16fa02c87dfecc
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-03-20 12:50:32 -0400

    Live Review Sections

---
+++ live_review_sections/week1/PrintStatements.java
@@ -0,0 +1,17 @@
+package week1;
+
+public class PrintStatements {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Every One, How Are You All Today?"); // appends new line at the end
+		System.out.println("Hi");  // new line
+		
+		System.out.println("-------------------------------------");
+		
+		System.out.print("Hello Every One, How Are You All Today?"); // does not append new line
+		System.out.print("Hi");
+
+	}
+
+}
---
+++ live_review_sections/week1/TellMeAboutMySelf.java
@@ -0,0 +1,24 @@
+package week1;
+
+public class TellMeAboutMySelf {
+	
+	public static void main(String[] args) {
+		   // FCFS
+		System.out.println("My name is Muhtar\n");
+		System.out.println("\tI live in Virginia");
+		System.out.println("I \"teach Java\"");
+		System.out.println("My dream job is 'to work in Cybertek'");
+		
+		
+	}
+
+}
+
+/*
+        print:
+            your name
+            where do you live
+            what do you do
+            what's your dream job
+*/
+
