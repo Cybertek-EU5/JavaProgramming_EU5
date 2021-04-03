666f12b69e130bc79437e6069bc04f73e2539983
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-04-03 15:37:08 -0400

    week2

---
+++ live_review_sections/week2/IfStatements3.java
@@ -0,0 +1,32 @@
+package week2;
+
+public class IfStatements3 {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
+
+
+/*
+  write a program that can find the number of days in a month
+        (Assume that Feb has 28 days)
+        ex: 
+            month = 3
+        output:
+            31 days
+            month = 4
+        output:
+            30 days
+            month = 13
+        output:
+            Invalid month
+            
+        HINT: 
+            28 days: 2
+            30 days: 4,6,9,11
+            31 days: 1,3,5,7,8,10,12
+*/
