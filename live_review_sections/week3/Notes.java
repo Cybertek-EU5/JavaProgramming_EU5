12a471321b144c176e3b0745ed2a89d07e30c663
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-04-10 16:23:15 -0400

    week3
    Class Notes

--- live_review_sections/week3/Notes.java
+++ live_review_sections/week3/Notes.java
@@ -3,7 +3,7 @@ package week3;
 public class Notes {
 	
 	/*
-	 04/10/2021
+	04/10/2021
 Week3 Java Review:
 		1. Switch statements
 		2. Loops
@@ -85,24 +85,72 @@ Loops: repeated actions
 
 		do-while loop:
 
+			do{
+
+			}while(Condition);
+
+
+Nested Loop: loop inside another loop
+	when do we need it: when we need to repeat a loop
+
+	1 2 3 4 5  // 1 for loop
+	1 2 3 4 5  // 1 for loop
+	1 2 3 4 5  // 1 for loop
+	1 2 3 4 5  // 1 for loop
+	1 2 3 4 5  // 1 for loop
+	1 2 3 4 5  // 1 for loop
+	1 2 3 4 5  // 1 for loop
+	1 2 3 4 5  // 1 for loop
+	1 2 3 4 5  // 1 for loop
+	1 2 3 4 5  // 1 for loop
+
+	==========================
+	total 10 for loops are being used ==> can be created by using one nested loop
+
+Branching statements:
+	break: used for exiting the loop & switch statement
+			when we need to forcefully exit the loop
+
+	continue: skip iteration, jumps to the enxt iteration. 
+				THERE MUST BE A LOOP
+
+	return: exits the method. can also be used to return a value
+
+
+Methods: Function. grouping aseries of codes
+
+	int n1 =10            isEven(n1);   
+	if(n1%2==0){
+		Even
+	}
+
+	int n2 = 20;          isEven(n2);
+	if(n1%2==0){
+		Even
+	}
+
+
+	Access-Modifier   specifier   return-Type   methodName(Parameter){
+				statements
+	}
+
+	Access-modifiers: public, private, protected, default
+			public: always accessible
+
+	specifier: static, final, abstract
+			static: can be called through class name
+
+	returnType:  void, any dataType
+			void: cannot return any value from the method
+			not-void: method returns a value
+
+	parameter: any dataType
+			a data that the method method MUST have in order to complete functionality
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		
 		
 	 */
 
