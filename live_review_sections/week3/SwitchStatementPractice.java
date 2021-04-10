ce3c019864947d87cceee1ce3b24f166d2bffa22
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-04-10 10:47:12 -0400

    week3

---
+++ live_review_sections/week3/SwitchStatementPractice.java
@@ -0,0 +1,48 @@
+package week3;
+
+public class SwitchStatementPractice {
+	
+	public static void main(String[] args) {
+		
+		/*
+		boolean r = "Hello" =="Hola";
+		boolean r2 = 10 == 10;
+		boolean r3 = 'A' == 'a';
+		
+		double d = 10d;  
+		double d1 = 10D;
+		double d2 = 10;
+		
+		float f = 10f;
+		long l = 10l;
+		*/
+		
+		// System.out.println(10 == "10");
+		// equal
+		int number = 20;
+		
+		switch(number) { //10
+				
+			case 10:
+					System.out.println("A");
+					break;  //exits the switch
+				
+			case 11:
+					System.out.println("B");
+					break;
+				
+			case 12:
+					System.out.println("C");
+					break;
+					
+			default:
+				System.out.println("D");
+			
+		}
+		
+		
+		
+		
+	}
+
+}
