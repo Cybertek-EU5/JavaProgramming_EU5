16bbc09d28e020e0fc7863d6a0ab642de59e215c
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-04-10 12:29:54 -0400

    week3

---
+++ live_review_sections/week3/LoopPractice.java
@@ -0,0 +1,21 @@
+package week3;
+
+public class LoopPractice {
+	
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 5; i++) {  // i: 1, 2, 3, 4, 5, 6
+			System.out.println("Hello World");
+		}
+		
+		
+		System.out.println("=====================================");
+		for(int i = 1; i <= 5; i+=2 ) { // i: 1, 3, 5, 7
+			System.out.println("Hello World");  // hello world, hello world, hello world
+		}
+		
+		
+		
+	}
+
+}
---
+++ live_review_sections/week3/LoopPractice2.java
@@ -0,0 +1,51 @@
+package week3;
+
+public class LoopPractice2 {
+	
+	public static void main(String[] args) {
+		System.out.print("Even Numbers: ");
+		// 0 2 4 6 8 10 .... 1000
+		for(int i = 0; i <= 1000;  i += 2) {
+			System.out.print(i+" ");
+		}
+		
+		System.out.println("\n--------------------------");
+		
+		System.out.print("Odd Numbers: ");
+		// 1 3 5 7 9 11 13 .... 999
+		for(int i = 1; i < 1000; i += 2 ) {
+			System.out.print(i+" ");
+		}
+		
+		System.out.println("\n--------------------------");
+		System.out.print("Even Numbers: ");
+		for(int i =0; i <= 100; i++) {  // i: 0,1,2,3,4,5....1000
+			if(i % 2 == 0) {
+				System.out.print(i+" ");
+			}
+		}
+		
+		System.out.println("\n--------------------------");
+		System.out.print("Odd Numbers: ");
+		for(int i =0; i <= 100; i++) {  // i: 0,1,2,3,4,5....1000
+			if(i % 2 != 0) {
+				System.out.print(i+" ");
+			}
+		}
+		
+		
+		
+	}
+
+}
+
+/*
+ Task3:
+        1. write a program that can print all even numbers between 0 ~ 1000
+        2. write a program that can print all odd numbers between 0 ~ 1000
+        3. write a program that can print a~z
+        4. write a program that can print A~Z
+        5. write a program that can print z~a
+        6. write a program that can print Z~A
+
+ */
