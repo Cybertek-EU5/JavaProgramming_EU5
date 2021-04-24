7cd8d2331984a39f5e294361e95d5da7400b7591
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-04-24 12:23:50 -0400

    week5

--- live_review_sections/week5/Calculator.java
+++ live_review_sections/week5/Calculator.java
@@ -6,39 +6,54 @@ public class Calculator {
 
 	public static void main(String[] args) {
 
-		Scanner scan = new Scanner(System.in); //  \n\n
+		Scanner scan = new Scanner(System.in); // \n\n
 
-		
-		while(true) {
+		while (true) {
 			System.out.println("Enter your first number");
-			int f = scan.nextInt();  //10
+			int f = scan.nextInt(); // 10
 
 			System.out.println("Enter your second number");
-			int s = scan.nextInt();  //20
+			int s = scan.nextInt(); // 20
 
-			System.out.println("Sum: " + (f + s));
+			System.out.println("Enter a math operator");
+			char operator = scan.next().charAt(0); // +, -, *, /
 
-			scan.nextLine(); // \n\n , TAKES EVERYTHING, INCLUDING ENTER 
-			
+			switch (operator) {
+				case '-': System.out.println("Subtraction: "+(f-s));
+						 break;
+						 
+				case '+': System.out.println("Addition: "+(f+s));
+				 		break;
+				 			
+				case '*': System.out.println("Multiplication: "+(f*s));
+	 					break;
+	 					
+				case '/': System.out.println("Division: "+(f/s));
+	 					break;
+				
+	 			default:
+	 				System.out.println("Invalid Operator");
+
+			}
+
+			scan.nextLine(); // \n\n , TAKES EVERYTHING, INCLUDING ENTER
+
 			System.out.println("Do you want to continue?");
 			String a = scan.nextLine().toLowerCase();
-			
-			while( ! (a.equals("yes") || a.equals("no")) ) { // if the answer is invalid
+
+			while (!(a.equals("yes") || a.equals("no"))) { // if the answer is invalid
 				System.out.println("Invalid Entry, pelase re-enter");
-				 a = scan.nextLine().toLowerCase();
+				a = scan.nextLine().toLowerCase();
 			}
-			
-			
-			if(a.equals("no")) {
+
+			if (a.equals("no")) {
 				System.out.println("Test Completed");
 				break;
 			}
-			
+
 		}
-		
-		
-		
-		
+
+		scan.close();
 
 	}
 
