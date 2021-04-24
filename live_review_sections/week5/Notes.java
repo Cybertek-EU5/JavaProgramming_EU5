5a8ae176d7cdb4819aea90ef0577e2d21258f9f4
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-04-24 16:07:23 -0400

    week5

---
+++ live_review_sections/week5/Notes.java
@@ -0,0 +1,68 @@
+package week5;
+
+public class Notes {
+
+	/*
+	 04/24/2021
+Java Review:
+	1. Class & Object Practice
+	2. String methods
+	3. String & loops practices
+	4. Nested Loop Practice
+
+
+Assignment Task:
+	LogIn:
+		
+		String expectedUserName = "Cybertek";
+		String expectedPassWord = "Cybertek123";
+
+
+		1. Ask user to enter Cybertek username
+		2. Ask user to enter Cybertek password
+
+		3. if username & password both matches with the expected username and passwords
+					output:
+							logged in
+
+		4. if username || password is not matched, ask the user to re-enter
+				USER WILL ONLY HAVE 3 chances, 
+
+				after 3rd attempt if the username || password still invalid:
+					account should be locked
+
+come back at 2pm US EST
+
+
+	10 * 3 = 30
+	10 + 10 + 10 = 30;
+
+	10 / 3 ===> 3 with a remainder of 1
+
+
+	10 - 3 = 7
+	7 - 3 = 4;
+	4 - 3 = 
+
+	10 - 3 - 3 -3 =1
+
+
+FormatFirstLastNames:
+		
+		ENter firstName:
+			cYbErTeK ==> "c".toUpperCase + "YbErTeK".toLower ==> Cybertek
+						substring(0,1).toUpperCase  +  substring(1)toLowerCase
+
+		Enter lastName:
+			SCHool
+
+		Cybertek School
+
+
+
+
+
+
+
+	 */
+}
