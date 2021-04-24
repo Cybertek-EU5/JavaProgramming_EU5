49d76dcd3f38cadf50387e844ec7e92eb887704a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-04-24 15:34:36 -0400

    week5

--- live_review_sections/utilities/StringUtility.java
+++ live_review_sections/utilities/StringUtility.java
@@ -21,6 +21,46 @@ public class StringUtility {
 	}
 	
 	
+	// removes the duplicates from string
+		public static String removeDuplicates(String str) {
+
+			String result = ""; // ab
+
+			for (int i = 0; i < str.length(); i++) {
+				if (!result.contains("" + str.charAt(i))) {
+					result += str.charAt(i);
+				}
+			}
+
+			return result;
+		}
+
+		// returns the frequency of ch from str. frequency(String, char) ("aaa", 'a') ==> 3
+		public static int frequency(String str, char ch) {
+			int count = 0;
+			for (int i = 0; i < str.length(); i++) {
+				if (str.charAt(i) == ch) {
+					count++;
+				}
+			}
+			return count;
+		}
+
+		// returns the frequency of the characters from the string
+		public static String frequencyOfChars(String str) {
+			String nonDup = removeDuplicates(str); // abc
+
+			String result = ""; // a3b2c4
+
+			for (int i = 0; i < nonDup.length(); i++) { // used for iterating the characters of nonDup
+				int count = frequency(str, nonDup.charAt(i)); // getting the frequency of each character
+				result += nonDup.charAt(i) + "" + count;
+			}
+			return result;
+		}
+
+	
+	
 	
 
 }
--- live_review_sections/week5/FrequencyOfCharacters.java
+++ live_review_sections/week5/FrequencyOfCharacters.java
@@ -11,11 +11,13 @@ public class FrequencyOfCharacters {
 			
 			char ch = str.charAt(j);  // each char of str,
 			int count = 0; // for the frequency of ch
+			
 			for(int i = 0; i< str.length(); i++) { //i: index numbers of str
 				if(ch == str.charAt(i)) { // ch will be compared with each characters of str
 					count++ ;
 				}
 			}
+			
 			if(!result.contains(""+ch)) {  // characters should not duplicated in the result
 				result += ch+""+count;
 			}
