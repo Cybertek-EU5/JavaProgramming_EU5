fdd227d0d734c55839aeb2cbcadfbdba4a1c7fcf
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-04-24 14:12:50 -0400

    week5

--- live_review_sections/week5/Division.java
+++ live_review_sections/week5/Division.java
@@ -2,4 +2,35 @@ package week5;
 
 public class Division {
 
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 0;
+
+		/*
+		 * 10 - 3 - 3 - 3 = 1
+		 * 
+		 * 10 - 4 - 4 = 2
+		 * 
+		 * 10 - 2 - 2 - 2 - 2 - 2 = 0
+		 * 
+		 */
+
+		if (b != 0) {
+			int count = 0;
+
+			while (a >= b) {
+				a -= b;
+				count++;
+			}
+
+			System.out.println(count + " with a remainder of " + a);
+		}else {
+			System.out.println("Invalid Number for denominator");
+		}
+		
+		
+
+	}
+
 }
