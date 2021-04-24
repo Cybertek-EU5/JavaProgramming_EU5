a643d48ceb9ec0a3927bac5dea573c7bfe167d5d
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-04-24 10:58:56 -0400

    week5

---
+++ live_review_sections/week5/Pizza.java
@@ -0,0 +1,55 @@
+package week5;
+
+public class Pizza {
+	
+	public String size;
+	public int cheeseTopping;
+	public int pepperoniTopping;
+	
+	public void customizeOrder(String pizzaSize, int cheese, int pepperoni) {
+		size = pizzaSize;
+		cheeseTopping = cheese;
+		pepperoniTopping = pepperoni;
+	}
+	
+	public double calculatePrice() {
+		int startingPrice = (size.equals("small"))? 10 
+				:(size.equals("medium")) ? 12 : 14;
+		
+		int totalPriceOfCheese = 1 * cheeseTopping;
+		int totalPriceOfPepproni = 2 * pepperoniTopping;
+		
+		return startingPrice + totalPriceOfCheese + totalPriceOfPepproni;
+	}
+
+	
+	public String toString() {
+		return "Pizza [size=" + size 
+				+ ", cheeseTopping=" + cheeseTopping 
+				+ ", pepperoniTopping=" + pepperoniTopping
+				+", total price=$"+calculatePrice() + "]";
+	}
+	
+	
+	
+	
+
+}
+/*
+Pizza:
+            attribute:
+                size
+                cheeseTopping
+                pepperoniTopping
+
+            Actions:
+                setInfo(), calculatePrice(), toString()
+                assume that:
+                    size:
+                        small ==> $10
+                        medium ==> $12
+                        large ==> $14
+
+                    one cheese topping 1$
+                    one pepperoni topping 2$
+*/
