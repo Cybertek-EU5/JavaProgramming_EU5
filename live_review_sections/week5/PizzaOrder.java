964a0991b3d23f710e50c308cf21068ea2839e15
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-04-24 11:18:35 -0400

    week5

--- live_review_sections/week5/PizzaOrder.java
+++ live_review_sections/week5/PizzaOrder.java
@@ -18,7 +18,15 @@ public class PizzaOrder {
 		
 		System.out.println("----------------------------------------------------");
 		// order 159 pizza: medium, 2, 3
+		double totalPrice = 0;
 		
+		for(int i=1; i <= 159; i++) {
+			Pizza order = new Pizza();
+			order.customizeOrder("medium", 2, 3);
+			totalPrice += order.calculatePrice();
+		}
+		
+		System.out.println(totalPrice);
 		
 		
 	}
