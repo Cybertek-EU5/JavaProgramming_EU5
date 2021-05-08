05ea7752eb6bd2fca16777a063e99134220e7534
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-08 10:52:06 -0400

    week7

--- live_review_sections/utilities/ArraysUtility.java
+++ live_review_sections/utilities/ArraysUtility.java
@@ -99,6 +99,71 @@ public class ArraysUtility {
 		return result;
 	}
 	
+	// merges two arrays of int
+	public static int[] merge(int[] arr1, int[] arr2) {
+		int[] arr3 = new int[arr1.length + arr2.length];  //Make sure that this array can contain all the elements of arr1 & arr2
+		int i = 0; // will represents the index numbers of third array
+		
+		for (int each : arr1) {
+			  arr3[i++]=each;
+		}
+		
+		for (int each : arr2) {
+			arr3[i++] = each;
+		}
+		return arr3;
+	}
+	
+	
+	// merges two arrays of double
+	public static double[] merge(double[] arr1, double[] arr2) {
+		double[] arr3 = new double[arr1.length + arr2.length];  //Make sure that this array can contain all the elements of arr1 & arr2
+		int i = 0; // will represents the index numbers of third array
+		
+		for (double each : arr1) {
+			  arr3[i++]=each;
+		}
+		
+		for (double each : arr2) {
+			arr3[i++] = each;
+		}
+		return arr3;
+	}
+	
+	
+	// merges two arrays of char
+	public static char[] merge(char[] arr1, char[] arr2) {
+		char[] arr3 = new char[arr1.length + arr2.length];  //Make sure that this array can contain all the elements of arr1 & arr2
+		int i = 0; // will represents the index numbers of third array
+		
+		for (char each : arr1) {
+			  arr3[i++]=each;
+		}
+		
+		for (char each : arr2) {
+			arr3[i++] = each;
+		}
+		return arr3;
+	}
+	
+	// merges two arrays of String
+		public static String[] merge(String[] arr1, String[] arr2) {
+			String[] arr3 = new String[arr1.length + arr2.length];  //Make sure that this array can contain all the elements of arr1 & arr2
+			int i = 0; // will represents the index numbers of third array
+			
+			for (String each : arr1) {
+				  arr3[i++]=each;
+			}
+			
+			for (String each : arr2) {
+				arr3[i++] = each;
+			}
+			return arr3;
+		}
+
+	
+	
+	
 	
 
 }
--- live_review_sections/week7/MergeTwoArrays.java
+++ live_review_sections/week7/MergeTwoArrays.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class MergeTwoArrays {
 	
 	public static void main(String[] args) {
-		int[] arr1 = {1,2,3};
-		int[] arr2 = {4,5,6,7};
+		int[] arr1 = {1,2,3,4,5,6,7,8,9,10};
+		int[] arr2 = {11,12,13,14,15,16,17,18};
 		
 		
 		int[] arr3 = new int[arr1.length + arr2.length];  //Make sure that this array can contain all the elements of arr1 & arr2
@@ -22,8 +22,29 @@ public class MergeTwoArrays {
 		
 		
 		System.out.println( Arrays.toString( arr3) );
+		System.out.println("-------------------------------------------------------------");
+		
+		String[] group1 = {"Bilal", "Kunduz", "Berk"};		
 		
 		
 	}
+	
+	
+	public static int[] merge(int[] arr1, int[] arr2) {
+		int[] arr3 = new int[arr1.length + arr2.length];  //Make sure that this array can contain all the elements of arr1 & arr2
+		int i = 0; // will represents the index numbers of third array
+		
+		for (int each : arr1) {
+			  arr3[i++]=each;
+		}
+		
+		for (int each : arr2) {
+			arr3[i++] = each;
+		}
+		return arr3;
+	}
+	
+	
+	
 
 }
