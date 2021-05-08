698d7e8b68e44dd1fd36157d6f97a09b7c9e69a5
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-08 16:15:09 -0400

    week7

---
+++ live_review_sections/week7/Notes.java
@@ -0,0 +1,74 @@
+package week7;
+
+public class Notes {
+	
+	/*
+	 05/08/2021
+Review Topics: Arrays
+			   Arrays Utility
+			   Wrapper Class
+	   
+
+so far: Array, loop, Methods, Class....
+
+3 data structure:
+		Array (primitives & non-primitives)
+		Collection (non-primitives)
+		Map (non-primitives)
+
+
+Arrays: fixed size
+		can be multi-dimensional
+		supports primitives & non-primitives
+
+	single-dimensional array: to store elements
+				int arr1D = {1,2,3,4,5}
+
+	multi-dimensional array: to store arrays
+							n dimensional array: conatins (n-1) dimensional array
+
+					2D array: 
+							{1,2,3}
+							{4,5,6}
+							{7,8,9}
+
+							int[][] arr2D = { {1,2,3}, {4,5,6}, {7,8,9} };
+
+					3D array: 
+							{{1,2,3}, {4,5,6}}
+							{{7,8}, {9, 12}}
+
+							int[][][] arr3D = {  {{1,2,3}, {4,5,6}} ,    {{7,8}, {9, 12}}  };
+
+
+Wrapper Class:
+		
+		autoboxing: primitive to wrapper
+		unboxing: wrapepr to primitive
+
+		parse methods: string to primitive
+		valueOf methods: string to wrapper class
+
+
+
+come back at 2:15pm US EST
+
+
+jar: java availble resources
+
+create project:
+		file -> new -> Java project
+
+how to add external lib to java project:
+		right click project -> properties -> 
+
+
+
+
+
+
+
+
+	 */
+
+}
