15703675a0dbc99f78ea8d9d3b8417801261fb8c
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-15 12:56:58 -0400

    week8

--- live_review_sections/week8/Walmart.java
+++ live_review_sections/week8/Walmart.java
@@ -5,14 +5,29 @@ public class Walmart {
 	public static void main(String[] args) {
 		
 		Item[] items = {
-				new Item("Toilet Paper", 2, 100),
-				new Item("cucumber", 1.59, 3),
+				new Item("Toilet Papers", 2, 100),
+				new Item("cucumbers", 1.59, 3),
 				new Item("tomatoes", 1.99, 7),
-                new Item("potatoes", 3.99, 5),
+                new Item("potatoes", 3.99, 500),
                 new Item("shirt", 12.99, 3),
-                new Item("tv", 200.99, 1),
+                new Item("TV", 200.99, 1),
 		};
 		
+		double totalPriceBeforeTax = 0;
+		int largestQuntity = 0;
+		String nameOfItem = "";
+		
+		
+		for (Item item : items) {
+			totalPriceBeforeTax	+= item.calCost();
+			if(item.quantity > largestQuntity) {
+				largestQuntity = item.quantity;
+				nameOfItem = item.name;
+			}
+		}
+		
+		System.out.println("total: $"+totalPriceBeforeTax);
+		System.out.println(nameOfItem +" has the largest quqntity");
 		
 		
 		
