579e9dd44c94d03fb92845b565aeb2136e5dea9e
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-15 15:50:45 -0400

    week8

--- live_review_sections/week8/ArrayListPractice4.java
+++ live_review_sections/week8/ArrayListPractice4.java
@@ -19,6 +19,55 @@ public class ArrayListPractice4 {
 		Collections.replaceAll(list, 'A', 'E');
 		System.out.println(list);
 		
+		int frequencyOfE = Collections.frequency(list, 'E');
+		System.out.println(frequencyOfE);
+		
+		System.out.println("------------------------------------");
+		
+		ArrayList<Integer> numbers = new ArrayList<>(  );
+		numbers.addAll(Arrays.asList(1,2,3,1,2,3,4,5,5,6,7,7));
+		
+		System.out.println(numbers);
+		
+		for(Integer each : numbers) {
+			if( Collections.frequency(numbers, each) == 1 ) { // if frequency of the element in arraylist is 1, then it's unique
+				System.out.println(each);
+			}
+		}
+		
+		System.out.println("------------------------------------");
+		
+		String str = "aabccd";
+		String unique = "";
+		
+		ArrayList<String> chars = new ArrayList<>( Arrays.asList(str.split("")) );
+		
+		for(String each : chars) {
+			int f = Collections.frequency(chars, each);
+			if(f == 1) {
+				unique += each;
+			}
+		
+		}
+		
+		
+		System.out.println(unique);
+		
+		System.out.println("--------------------------------------------");
+		
+		String s = "aabbbcccc";
+		String result = "";	 // a2b3c4
+		
+		ArrayList<String> characters = new ArrayList<>( Arrays.asList( s.split("") ) );
+		
+		for(String each : characters) {
+			int f = Collections.frequency(characters, each);
+			if(!result.contains(each)) {
+				result += each+f;
+			}
+		}
+		
+		System.out.println(result);
 		
 		
 		
