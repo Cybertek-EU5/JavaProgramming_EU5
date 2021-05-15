04b3e7b7ba4527f76eadddc9dfa5faf6a2f440bd
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-15 12:38:48 -0400

    week8

---
+++ live_review_sections/week8/Item.java
@@ -0,0 +1,26 @@
+package week8;
+
+public class Item {
+	
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public Item(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	
+	public double calCost() {
+		return price * quantity;
+	}
+
+
+	public String toString() {
+		return "Item [name=" + name + ", price=" + price + ", quantity=" + quantity + "]";
+	}
+	
+
+}
