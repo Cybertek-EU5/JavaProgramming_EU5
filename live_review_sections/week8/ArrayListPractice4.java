005afd1638de4c9b05a7e506d397681a2f1cc7a6
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-15 15:12:57 -0400

    week8

--- live_review_sections/week8/ArrayListPractice4.java
+++ live_review_sections/week8/ArrayListPractice4.java
@@ -1,7 +1,23 @@
 package week8;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ArrayListPractice4 {
 	public static void main(String[] args) {
+		ArrayList<Character> list = new ArrayList<>();	
+		list.addAll(  Arrays.asList('D', 'B', 'C','A', 'A','A') );
+		
+		Collections.sort(list); // sorts in ascending order
+		System.out.println(list);
+		
+		Collections.swap(list,  list.size()-1, 0 );
+		
+		System.out.println(list);
+		
+		Collections.replaceAll(list, 'A', 'E');
+		System.out.println(list);
 		
 		
 		
