518d31c21740d9512fee3ad8970cbe27aedbf725
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-15 13:10:47 -0400

    week8

---
+++ live_review_sections/week8/Notes.java
@@ -0,0 +1,124 @@
+package week8;
+
+public class Notes {
+	/*
+	 05/15/2021
+Review Topics:
+			Constructors
+			ArrayList
+			ArrayList vs Array
+			Collections Utility
+
+methods:
+		AM  specifier  returnType  methodName(parameter){
+						(mandatory)
+
+		}
+
+		AM: public, private, default, protected
+		specifier: static
+		returnType: void,  any dataType
+		method name: any name
+		parameter: any dataType
+
+Constructor: special method. used for instantiate/create object
+
+			execution of the constructor depends on the creation of object
+
+			EVERY CLASSS MUST HAVE
+
+	declearing:
+
+			AM  className(parameter){
+
+			}
+	
+
+
+creating object:
+			
+			new  ExistingConstructor
+		
+		new dog("Lucy", "Husky", "White");
+
+
+		dog dog2 = new dog();
+		dog2.name = "Joe"
+		dog2.breed = "Husky";
+		dog2.color = "White";
+
+
+
+this: refers to the object instances
+	this. : we only use it to call instance variable if local variable is having the same name as instance variable
+
+
+
+Employee:
+	attribute
+		name, jobTitle, ID, salary
+
+	add a constructor to initialize all the fields
+
+	toString()
+
+
+Item:
+	attributes:
+		name, unitPrice, quantity
+
+	add a constructor to initialize all the fields
+
+	toString()
+	calCost(): calculates toral price
+
+Task:
+	Car:
+		attributes:
+			brand, model, year, color, price
+
+		add a constructor to initialize all the fields
+
+		toString()
+
+	CarShop:
+
+		create an array of cars that has 5 objects of Car
+
+		recall:
+			TOYOTA 2008 ~ 2010
+			BMW 2011 ~ 2014
+
+
+Task2:
+	Carpet:
+		attributes:
+			width, length, unitPrice, isPersian
+
+		add a constructor to initialize all the fields
+
+		methods:
+			calcCost(): returns the total price of the carpet
+
+					totalPrice = width * length * unit price
+
+					if the carpet is persian carpet: + 300
+
+
+
+
+
+come back at:   12:30 pm  EST 
+
+
+
+
+
+
+
+
+
+		
+	 */
+
+}
