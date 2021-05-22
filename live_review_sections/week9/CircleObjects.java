3e58c92f6ec198a2063b0f7e83aff0190a227811
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-22 12:43:15 -0400

    week9

---
+++ live_review_sections/week9/CircleObjects.java
@@ -0,0 +1,17 @@
+package week9;
+
+public class CircleObjects {
+	
+	public static void main(String[] args) {
+		
+		Circle circle = new Circle(5);
+		
+		System.out.println(circle.radius);
+		System.out.println(circle);
+		
+	}
+	
+	
+	
+
+}
