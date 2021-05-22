2c4bb5206a82a8f6c0f580d23569f5c9742f3042
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-22 13:00:24 -0400

    week9

--- live_review_sections/week9/Circle.java
+++ live_review_sections/week9/Circle.java
@@ -2,18 +2,35 @@ package week9;
 
 public class Circle {
 	
-	public double radius;
+	private double radius;
 	
 	public Circle(double radius) {
+		setRadius(radius);
+	}
+
+	
+	public double getRadius() {
+		return radius;
+	}
+
+
+	public void setRadius(double radius) {
+		
+		if(radius <=0 ) {
+			System.err.println("Invalid Radius");
+			return;
+		}
+	
 		this.radius = radius;
 	}
-	
-	
+
+
 	public double getArea() {
 		return Math.pow(radius, 2) * Math.PI;
 	}
 	
 	public double getPerimeter() {
+		
 		return radius * 2 * Math.PI;
 	}
 
--- live_review_sections/week9/CircleObjects.java
+++ live_review_sections/week9/CircleObjects.java
@@ -5,9 +5,21 @@ public class CircleObjects {
 	public static void main(String[] args) {
 		
 		Circle circle = new Circle(5);
+		//circle.radius = -1;
+		///System.out.println(circle.radius);
 		
-		System.out.println(circle.radius);
+		//circle.setRadius(-1);
 		System.out.println(circle);
+		
+		System.out.println("----------------------");
+		
+		Circle circle2 = new Circle(1);
+		System.out.println(circle2);
+		
+		circle2.setRadius(2);
+		System.out.println(circle2);
+		
+		
 		
 	}
 	
