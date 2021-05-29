81a1d5b77a88adb94d37eb8c2044d8ba3c14646e
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-29 11:05:23 -0400

    week10

---
+++ live_review_sections/week10/CybertekStudent.java
@@ -0,0 +1,38 @@
+package week10;
+
+public class CybertekStudent {
+	
+	public String name;
+	public int age;
+	public String batch;
+	
+	public CybertekStudent(String name, int age, String batch) {
+		this.name = name;
+		this.age = age;
+		this.batch = batch;
+	}
+	
+	
+	public static String schoolName;
+	public static String programmingLanguage;
+	
+	static {
+		schoolName = "Cybertek School";
+		programmingLanguage = "Java";
+	}
+	
+	public void printName() {
+		System.out.println("Student Name: "+name);
+	}
+	
+	public static void printSchoolName() {
+		System.out.println("School Name: "+schoolName);
+	}
+	
+	public void printNameAndLanguage() {
+		System.out.println("Student Name: "+name);
+		System.out.println("Programming Language: "+programmingLanguage);
+	}
+	
+
+}
---
+++ live_review_sections/week10/StudentObjects.java
@@ -0,0 +1,5 @@
+package week10;
+
+public class StudentObjects {
+
+}
