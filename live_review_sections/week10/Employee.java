587d43d5d0f7315113b3b670d1df17080a1214f8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-29 12:03:36 -0400

    week10

--- live_review_sections/week10/Employee.java
+++ live_review_sections/week10/Employee.java
@@ -9,12 +9,33 @@ public class Employee {
 	public int employeeId;
 	public double salary;
 	
+	public static boolean isEmployeed = true;
+	
 	public Employee(String name, String gender, String jobTitle, int employeeId, double salary) {
 		this.name = name;
 		this.gender = gender;
 		this.jobTitle = jobTitle;
 		this.employeeId = employeeId;
 		this.salary = salary;
+	}
+	
+	
+	public void eat() {
+		System.out.println(name+" is eating");
+	}
+	
+	public void sleep() {
+		System.out.println(name+" is sleeping");
+	}
+	
+	public void work() {
+		System.out.println(name +" is working");
+	}
+
+
+	public String toString() {
+		return "Employee [name=" + name + ", gender=" + gender + ", jobTitle=" + jobTitle + ", employeeId=" + employeeId
+				+ ", salary=" + salary + "]";
 	}
 	
 	
