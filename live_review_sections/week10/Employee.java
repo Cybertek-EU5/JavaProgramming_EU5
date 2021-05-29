fbd56bb6c545ad90bfda18db4ed0b041f90f2b30
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-29 11:59:43 -0400

    week10

---
+++ live_review_sections/week10/Employee.java
@@ -0,0 +1,30 @@
+package week10;
+
+public class Employee {
+	
+	public String name,
+				gender,
+				jobTitle;
+	
+	public int employeeId;
+	public double salary;
+	
+	public Employee(String name, String gender, String jobTitle, int employeeId, double salary) {
+		this.name = name;
+		this.gender = gender;
+		this.jobTitle = jobTitle;
+		this.employeeId = employeeId;
+		this.salary = salary;
+	}
+	
+	
+	
+	
+
+}
+
+/*
+create a class called Employee
+	name, gender, employeeID, JobTitle, Salary
+	eat(), sleep(), work(), toString()
+*/
