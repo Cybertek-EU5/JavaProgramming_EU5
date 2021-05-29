456d368efb59191b7580e81b61763f51acfef6a8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-05-29 12:29:39 -0400

    week10

---
+++ live_review_sections/week10/Scrumteam.java
@@ -0,0 +1,48 @@
+package week10;
+
+import java.util.ArrayList;
+
+public class Scrumteam {
+	
+	public String PO, BA, SM;
+	
+	public ArrayList<Tester> testers = new ArrayList<>();
+	public ArrayList<Developer> developers = new ArrayList<>();
+	
+	public Scrumteam(String pO, String bA, String sM) {
+		PO = pO;
+		BA = bA;
+		SM = sM;
+	}
+	
+	
+	
+	
+	
+	
+
+}
+/*
+Attributes:
+        PO, BA, SM
+        ArrayList<Tester> testersList = new ArrayList<>(),
+        ArrayList<Developer> devopsList = new ArrayList<>(),
+        
+
+Add a constructor that can set PO, BA, SM
+
+Actions:
+addTester(Tester tester): adds the given tester to the testers arraylist
+
+addTesters(Tester[] testers): adds the given testers to the testers arraylist
+
+addDeveloper(Developer developer): adds the given developer to the developers arraylist
+
+addDevelopers(Developer[] developers): adds the given developers to the developers arraylist
+
+removeTester(long employeeID): removes the given tester from the testers arraylist
+
+removeDeveloper(long employeeID): removes the developer from the developers arraylist
+
+toString(): prints number of tester,& developers,  PO name, SM name, BA name
+*/
