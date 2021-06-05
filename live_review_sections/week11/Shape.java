aba5e336e398683ec1b0ebc6edac3aec4223387f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-05 10:47:17 -0400

    week11

---
+++ live_review_sections/week11/Shape.java
@@ -0,0 +1,37 @@
+package week11;
+
+public class Shape {
+	
+	
+	public String name;
+	
+	public Shape(String name) {
+		this.name = name;
+	}
+	
+	
+	public double area() {
+		return 0;
+	}
+	
+	public double perimeter() {
+		return 0;
+	}
+
+
+
+	public String toString() {
+		return "Shape [name= " + name + ", area= " + area() + ", perimeter= " + perimeter() + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
+
+
+// circle , rectangle
