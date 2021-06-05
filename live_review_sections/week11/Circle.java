40f4367cab01fb0d2839b229ca7d5dbaeefd81db
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-05 11:06:40 -0400

    week11

---
+++ live_review_sections/week11/Circle.java
@@ -0,0 +1,28 @@
+package week11;
+
+public class Circle extends Shape {
+	
+	public double radius;
+	
+	public final static double PI;
+	
+	static {
+		PI = 3.14;
+	}
+	
+	public Circle(double radius){
+		 super("Circle");
+		 this.radius = radius;
+		
+	}
+	
+	
+	
+	
+
+}
+
+
+
+
+
