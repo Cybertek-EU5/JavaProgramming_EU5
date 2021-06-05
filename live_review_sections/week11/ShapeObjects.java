ddbf781ee48f57996baade467915146928521e78
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-06-05 12:32:40 -0400

    week11

---
+++ live_review_sections/week11/ShapeObjects.java
@@ -0,0 +1,24 @@
+package week11;
+
+public class ShapeObjects {
+	
+	/*
+	 	circle:  r=2.5
+	 	rectangle: w=10, l=20
+	 	square: side = 5
+	 */
+	
+	public static void main(String[] args) {
+		
+		Circle circle1 = new Circle(2.5);
+		
+		System.out.println(circle1);
+		
+		
+		
+	}
+	
+	
+	
+
+}
