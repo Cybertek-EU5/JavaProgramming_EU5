0af2af1ba86e228b7188cfdeaa7ee3f127459e72
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-10 10:58:50 -0400

    week14

--- live_review_sections/week14/ExceptionHandlings_TryCatch.java
+++ live_review_sections/week14/ExceptionHandlings_TryCatch.java
@@ -1,5 +1,7 @@
 package week14;
 
+import java.util.Arrays;
+
 public class ExceptionHandlings_TryCatch {
 	
 	public static void main(String[] args) {
@@ -22,11 +24,42 @@ public class ExceptionHandlings_TryCatch {
 		System.out.println("Test 1 Completed");
 		
 		
+		int[] number = {1,2,3,4,5};
+		
+		try {
+			System.out.println(number[10]);
+			System.out.println(Arrays.toString(number));
+			System.out.println("Try Block");
+		}catch(RuntimeException e) {
+			System.out.println("Catch Block");
+		}
+		
+		System.out.println("Test 2 completed");
+		
+		System.out.println("--------------------------------");
+		
+		try {
+			Thread.sleep(1000);
+			System.out.println("Try Block");
+		}catch (Exception e) {
+			System.out.println("Catch Block");
+		}
+		
+		System.out.println("Test 3 Completed");
 		
 		
 		
-		// 
+		System.out.println("---------------------------------");
 		
+		try {
+			System.out.println("Cybertek".charAt(200));
+			System.out.println("Try Block");
+		}catch(RuntimeException e) {
+			System.out.println("Catch Block");
+		//	System.exit(0); terminates the program
+		}finally {
+			System.out.println("Finally Block");
+		}
 		
 		
 		
