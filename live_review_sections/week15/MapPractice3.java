64d8cb833fa79f5195f58e82b51bafdd58e9aac8
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-17 15:08:15 -0400

    week15

--- live_review_sections/week15/MapPractice3.java
+++ live_review_sections/week15/MapPractice3.java
@@ -1,6 +1,7 @@
 package week15;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class MapPractice3 {
 	
@@ -13,6 +14,38 @@ public class MapPractice3 {
 	        employeeInfo.put("D",140000);
 	        employeeInfo.put("E",130000);
 	        employeeInfo.put("F",150000);
+	        
+	        
+	        String nameOfMaxSalary = "";
+	        int max  = 0;
+	        
+	        String nameOfMinSalary = "";
+	        int min = Integer.MAX_VALUE;
+	        
+	        
+	        
+	        for( Map.Entry<String, Integer>  each : employeeInfo.entrySet() ) {
+	        	//System.out.println(each);
+	        	String eachName = each.getKey();
+	        	int eachSalary = each.getValue();
+	        	
+	        	if(eachSalary > max ) {
+	        		max = eachSalary;
+	        		nameOfMaxSalary = eachName;
+	        	}
+	        	
+	        	if(eachSalary < min) {
+	        		min = eachSalary;
+	        		nameOfMinSalary = eachName;
+	        	}
+	        	
+	        }
+	        
+	        
+	        System.out.println(nameOfMaxSalary+" : "+max);
+	        System.out.println(nameOfMinSalary+" : "+min);
+	        
+	        
 		
 		
 		
