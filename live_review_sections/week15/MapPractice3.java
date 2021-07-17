c0a7164df7d6010d972522bfc5525311a45229db
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-07-17 14:57:54 -0400

    week15

---
+++ live_review_sections/week15/MapPractice3.java
@@ -0,0 +1,21 @@
+package week15;
+
+import java.util.LinkedHashMap;
+
+public class MapPractice3 {
+	
+	public static void main(String[] args) {
+		
+		 LinkedHashMap<String, Integer> employeeInfo = new LinkedHashMap<>();
+	        employeeInfo.put("A",120000);
+	        employeeInfo.put("B",130000);
+	        employeeInfo.put("C",110000);
+	        employeeInfo.put("D",140000);
+	        employeeInfo.put("E",130000);
+	        employeeInfo.put("F",150000);
+		
+		
+		
+	}
+
+}
